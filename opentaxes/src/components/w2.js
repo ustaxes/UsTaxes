bcbf6995d8748bac8285f7aa329bb51da269375e
Aidan Grimshaw <grimshawaidan@gmail.com>
2020-05-09 23:13:31 -0700

    changed theme

--- opentaxes/src/components/w2.js
+++ opentaxes/src/components/w2.js
@@ -1,17 +1,34 @@
 import React from 'react'
-import { Box } from "@material-ui/core"
+import { Box, createMuiTheme, ThemeProvider } from "@material-ui/core"
 import  W2EmployerInfo from './w2EmployerInfo'
+
+const theme = createMuiTheme({
+    palette: {
+        secondary: {
+            light: '#4f5b62',
+            main: '#263238',
+            dark: '#000a12',
+            contrastText: '#ffffff',
+        },
+        primary: {
+            light: '#66ffa6',
+            main: '#00e676',
+            dark: '#00b248',
+            contrastText: '#000000',
+        },
+    },
+});
 
 export default function W2() {
 
     return (
-        <div>
+        <ThemeProvider theme={theme}>
             <Box display="flex" justifyContent="center">
                 <Box display="flex" justifyContent="flex-start">
-                    <h1>Capital One Services LLC Wages (Form W-2)</h1>
+                    <h1>Wages (Form W-2)</h1>
                 </Box>
             </Box>
             <W2EmployerInfo/>
-        </div>
+        </ThemeProvider>
   )
 }
