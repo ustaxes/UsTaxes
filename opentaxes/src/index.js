4a5fa4e6c57bbba895fa47719ca7554379fafb6e
Aidan Grimshaw <grimshawaidan@gmail.com>
2020-06-22 13:54:10 -0600

    addded inital react router code

--- opentaxes/src/index.js
+++ opentaxes/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
