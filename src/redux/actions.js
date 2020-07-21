503f9ea97ce36914532437390a7777f13dfac05a
Aidan Grimshaw <grimshawaidan@gmail.com>
2020-07-20 20:02:25 -0700

    fixed selector issues

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -5,10 +5,11 @@ export const EMPTY_CART = 'EMPTY_CART'
 export const SAVE_FORM_DATA = 'SAVE_FORM_DATA'
 
 // saving form data to larger form data object with all information that has been saved to pages
-export function saveFormData(formData) {
+export function saveFormData(formData, key) {
     return {
         type: SAVE_FORM_DATA,
-        formData
+        formData,
+        key
     }
 }
 
