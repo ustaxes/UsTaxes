ed5ebe6b64637f118f731d49c1db78d80cd5f210
Aidan Grimshaw <grimshawaidan@gmail.com>
2020-12-15 17:16:01 -0800

    cleaned up commented functionality

--- src/pdfFiller/fill1040Fields.js
+++ src/pdfFiller/fill1040Fields.js
@@ -1,6 +1,3 @@
-// credit for the recurseAcroFieldKids and getRootAcroFields functions goes to Andrew Dillon
-// https://github.com/Hopding/pdf-lib/issues/349
-
 import fetch from 'node-fetch'
 import {
     drawImage,
@@ -22,43 +19,26 @@ import {
     PDFDict,
     PDFField,
     PDFCheckBox,
-    PDFAcroForm
+    PDFAcroForm,
+    PDFTextField,
+    StandardFonts
 } from 'pdf-lib'
 import flatFieldMappings from './1040flatFieldMappings'
 import { getAllDataFlat } from '../redux/selectors'
 import { store } from '../redux/store';
 
-function fillAcroTextField(
-    // pdfDoc,
-    acroField,
-    // fontObject,
+function fillPDField(
+    PDField,
     text,
-    // fontSize = 15,
 ) {
-    console.log(acroField)
-    acroField.setText(text)
-    // set text field
-    // acroField.set(PDFName.of('V'), PDFString.of(text));
-    // // set font properties
-    // acroField.set(PDFName.of('DA'), PDFString.of("/ZapfDingbats 10.00 Tf"))
-    // // acroField.set(PDFName.of('Ff'), PDFNumber.of(
-    // //     1 << 0 // Read Only
-    // //     |
-    // //     1 << 12 // Multiline
-    // // ));
-    
-    // // is the field a textbox?
-    // if (acroField.has(PDFName.of('AP'))){
-    //     // acroField.check()
-    //     // console.log( acroField.get(PDFName.of('T')).value)
-    //     acroField.set(PDFName.of('V'), PDFName.of('Yes'));
-    //     acroField.set(PDFName.of('AS'), PDFName.of('Yes'));
-        
-        // acroField.set(PDFName.of('DA'), PDFString.of("/HelveticaLTStd-Bold 10.00 Tf"))
-        // acroField.get(PDFName.of('FT')).get(PDFName.of('Btn'))
-        // console.log(acroField.get(PDFName.of('FT')))
-        // acroField.set(PDFName.of('FT'), PDFName.of('Yes'));
-    // }
+    if (PDField instanceof PDFTextField){
+        PDField.acroField.dict.dict.set(PDFName.of('DA'), PDFString.of("/HelveticaLTStd-Bold 12.00 Tf"))
+        PDField.setText(text)
+        // PDField.disableCombing()
+        // console.log(PDField)
+    } else if (PDField instanceof PDFCheckBox){
+        PDField.check()
+    }
 };
 
 // returns PDFDocument in the form of a Uint8Array
@@ -69,23 +49,26 @@ export async function fillPDF() {
     console.log(getAllDataFlat(store.getState()))
 
     const pdfDoc = await PDFDocument.load(await fetch('https://thegrims.github.io/UsTaxes/tax_forms/f1040.pdf').then(res => res.arrayBuffer()))
+    const timesRoman = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+    
     const formFields = pdfDoc.getForm().getFields()
-    formFields[0].check()
+    // fill fields with fieldNumber
+    formFields.forEach((PDField, index) => { if (PDField instanceof PDFTextField) {PDField.setText(index.toString())}})
+    // check all boxes
+    formFields.forEach((PDField, index) => { if (PDField instanceof PDFCheckBox) {PDField.check()}})
     console.log(formFields)
 
     Object.keys(flatFieldMappings).forEach(
         key => information[key] && 
-        fillAcroTextField(
+        fillPDField(
             formFields[
                 flatFieldMappings[key]
             ], 
             information[key]
         )
     )
-    
     formFields.forEach(formField => formField.enableReadOnly())
     
-
     const pdfBytes = await pdfDoc.save();
     return pdfBytes
 }
