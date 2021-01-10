12a6d91dc34488c8d41071c017adde43d3a6b056
Zak Patterson <pattersonzak@gmail.com>
2021-01-10 16:25:31 +0900

    Update tauri version with fixed writeBinaryFile (#32)

--- src/pdfFiller/pdfHandler.js
+++ src/pdfFiller/pdfHandler.js
@@ -1,19 +1,5 @@
-import { promisified } from 'tauri/api/tauri'
 import { save } from 'tauri/api/dialog'
-import { encodeToBase64 } from 'pdf-lib'
-
-/*
- * Remove this after next release of Tauri with this PR:
- * https://github.com/tauri-apps/tauri/pull/1136
- */
-async function writeBinaryFile ({ path, contents }, options = {}) {
-  return await promisified({
-    cmd: 'writeBinaryFile',
-    path: path,
-    contents: encodeToBase64(contents),
-    options
-  })
-}
+import { writeBinaryFile } from 'tauri/api/fs'
 
 export async function savePdf (contents) {
   if (window.__TAURI__ === undefined) {
