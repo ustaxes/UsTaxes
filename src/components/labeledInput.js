5ce18919579fa1a180568eaf70d2653994073a79
Zak Patterson <pattersonzak@gmail.com>
2021-01-13 16:04:25 +0900

    Add undefined option for state drop down (#40)
    So we don't assume people live in Alabama
    
    Fixes #39

--- src/components/labeledInput.js
+++ src/components/labeledInput.js
@@ -77,6 +77,7 @@ export function LabeledDropdown ({ label, dropDownData, valueMapping, keyMapping
                               native: true
                             }}
                         >
+                            <option value={undefined} />
                             {dropDownData.map(dropDownItem =>
                                 <option
                                     value={valueMapping ? valueMapping(dropDownItem) : dropDownItem}
