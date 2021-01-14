54e06f7fb46e191c2da62144f17d7c4c1aa2f312
Aidan Grimshaw <grimshawaidan@gmail.com>
2021-01-13 21:24:07 -0800

    added totalDeduction value

--- src/pdfFiller/calculateValues.js
+++ src/pdfFiller/calculateValues.js
@@ -6,5 +6,6 @@ export default function calculateValues () {
   const values = {}
   const information = getAllDataFlat(store.getState())
   values.totalIncome = information.income
+  values.totalDeduction = information.deduction
   store.dispatch(saveFormData(values, 'calculatedValues'))
 }
