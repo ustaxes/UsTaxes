fdd8ab2fa64d796de5d2fcba1eabb4e67fdb36a8
Zak Patterson <pattersonzak@gmail.com>
2021-02-02 15:17:32 +0900

    Fix checkbox alignment

--- src/components/input/LabeledCheckBox.tsx
+++ src/components/input/LabeledCheckBox.tsx
@@ -1,27 +1,37 @@
-import { Box, Checkbox, FormControlLabel } from '@material-ui/core'
+import { Checkbox, FormControl, FormLabel, FormControlLabel, FormGroup, makeStyles } from '@material-ui/core'
 import React, { ReactElement } from 'react'
 import { Controller } from 'react-hook-form'
 import { LabeledCheckBoxProps } from './types'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    margin: theme.spacing(3, 3, 0, 0)
+  }
+}))
+
 export function LabeledCheckBox (props: LabeledCheckBoxProps): ReactElement {
   const { label, name, value, setValue, control } = props
+
+  const classes = useStyles()
 
   return (
     <Controller
       name={name}
       as={
-        <Box alignContent="left">
-          <Box alignContent="left" display="block" paddingTop={2}>
-            {label}
-          </Box>
-          <Box alignContent="left" display="block" paddingTop={2}>
-            <FormControlLabel
-              control={<Checkbox name={name} checked={value} onChange={() => setValue(!value)} color="primary" />}
-              label={value ? 'Yes' : 'No'}
-              value={value}
-            />
-          </Box>
-        </Box>
+        <div className={classes.root}>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">{label}</FormLabel>
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox name={name} checked={value} onChange={() => setValue(!value)} color="primary" />}
+                label={value ? 'Yes' : 'No'}
+                value={value}
+              />
+            </FormGroup>
+          </FormControl>
+        </div>
       }
       control={control}
     />
