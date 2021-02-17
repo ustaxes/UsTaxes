6ebdaa216a7de4813e756c6a968efb43689fe2e9
Zak Patterson <pattersonzak@gmail.com>
2021-02-17 22:09:02 +0900

    Upgrade project to GPLv3 license, startup notice

---
+++ notice.js
@@ -0,0 +1,19 @@
+const notice = `
+    ustaxes helps you build your tax return forms for free
+    Copyright (C) 2021  Aidan Grimshaw and contributors
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+`
+
+console.log(notice)
