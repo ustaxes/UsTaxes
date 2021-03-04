ab3e119ecf7ab578e4bc2214eea2f505d20a5f8a
Zak Patterson <pattersonzak@gmail.com>
2021-03-04 16:26:16 +0900

    Fix tauri check to save PDF

--- src/pdfFiller/pdfHandler.js
+++ src/pdfFiller/pdfHandler.js
@@ -2,7 +2,7 @@ import { save } from 'tauri/api/dialog'
 import { writeBinaryFile } from 'tauri/api/fs'
 
 export async function savePdf (contents) {
-  if (window.__TAURI__ === undefined) {
+  if (window.__TAURI_INVOKE_HANDLER__ === undefined) {
     const blob = new Blob([contents], { type: 'application/pdf' })
     const blobURL = URL.createObjectURL(blob)
     window.open(blobURL)
