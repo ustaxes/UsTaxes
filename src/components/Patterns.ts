a0158e09e73373bdc9912800a906d758d96d2190
Aidan Grimshaw <grimshawaidan@gmail.com>
2021-03-19 00:07:52 -0700

    modify description text, expand regex digit range for bank account

--- src/components/Patterns.ts
+++ src/components/Patterns.ts
@@ -10,37 +10,37 @@ export const Patterns: {[name: string]: PatternConfig} = {
     description: 'Input should only include letters and spaces'
   },
   zip: {
+    mask: '99999-9999',
     regexp: /[0-9]{5}-[0-9]{4}/,
-    description: 'Input should be filled with 9 numbers',
-    mask: '99999-9999'
+    description: 'Input should be filled with 9 digits'
   },
   ssn: {
     mask: '999-99-9999',
     regexp: /[0-9]{3}-[0-9]{2}-[0-9]{4}/,
-    description: 'Input should be filled with 9 numbers'
+    description: 'Input should be filled with 9 digits'
   },
   ein: {
     mask: '99-9999999',
     regexp: /[0-9]{2}-[0-9]{7}/,
-    description: 'Input should be filled with 9 numbers'
+    description: 'Input should be filled with 9 digits'
   },
   currency: {
     regexp: /^[0-9]+$/,
-    description: 'Input should be filled with numbers only'
+    description: 'Input should be filled with digits only'
   },
   bankAccount: {
-    mask: '999999999999',
-    regexp: /[0-9]{10}|[0-9]{11}|[0-9]{12}/,
-    description: 'Input should be filled with 10-12 numbers'
+    mask: '99999999999999999',
+    regexp: /[0-9]{4,17}/,
+    description: 'Input should be filled with 4-17 digits'
   },
   bankRouting: {
     mask: '999999999',
     regexp: /[0-9]{9}/,
-    description: 'Input should be filled with 10 numbers'
+    description: 'Input should be filled with 10 digits'
   },
   usPhoneNumber: {
+    mask: '999-999-9999',
     regexp: /[0-9]{3}-[0-9]{3}-[0-9]{4}/,
-    description: 'Input should be filled with 10 numbers',
-    mask: '999-999-9999'
+    description: 'Input should be filled with 10 digits'
   }
 }
