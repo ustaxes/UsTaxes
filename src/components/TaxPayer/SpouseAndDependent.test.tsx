9d28301a7df5e4b419653bc1b6ca1be095fdd89a
jswiss <swissjoshua@gmail.com>
2021-03-24 22:08:46 +0000

    add two test cases

--- src/components/TaxPayer/SpouseAndDependent.test.tsx
+++ src/components/TaxPayer/SpouseAndDependent.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { Provider } from 'react-redux'
+import userEvent from '@testing-library/user-event'
 
 import { SpouseInfo } from './SpouseAndDependent'
 import { store } from '../../redux/store'
+import { act } from 'react-dom/test-utils'
 
 describe('SpouseInfo', () => {
   it('renders an `Add` button when no spouse has been added', () => {
@@ -25,5 +27,65 @@ describe('SpouseInfo', () => {
 
     const ssnLabel = screen.queryByLabelText('SSN / TIN')
     expect(ssnLabel).not.toBeInTheDocument()
+  })
+  it('renders form elements when `Add` button is clicked', () => {
+    render(
+    <Provider store={store}>
+      <SpouseInfo />
+    </Provider>
+    )
+
+    const addButton = screen.getByRole('button', {
+      name: /Add/
+    })
+
+    fireEvent.click(addButton)
+
+    screen.getByText('First Name and Initial')
+    screen.getByText('Last Name')
+    screen.getByText('SSN / TIN')
+
+    screen.getByRole('button', {
+      name: /Add/
+    })
+
+    screen.getByRole('button', {
+      name: /Close/
+    })
+  })
+
+  it('saves a spouse when all fields are entered', async () => {
+    render(
+    <Provider store={store}>
+      <SpouseInfo />
+    </Provider>
+    )
+
+    const addButton = screen.getByRole('button', {
+      name: /Add/
+    })
+
+    fireEvent.click(addButton)
+
+    const createButton = screen.getByRole('button', {
+      name: /Add/
+    })
+
+    screen.getByRole('button', {
+      name: /Close/
+    })
+
+    const inputs = screen.getAllByRole('textbox')
+    const [firstNameInput, lastNameInput, ssidInput] = inputs
+
+    userEvent.type(firstNameInput, 'Sally K')
+    userEvent.type(lastNameInput, 'Ride')
+    userEvent.type(ssidInput, '111111111')
+
+    act(() => {
+      fireEvent.click(createButton)
+    })
+
+    await screen.findByText('Sally K Ride')
   })
 })
