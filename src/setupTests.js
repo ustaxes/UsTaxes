93c2b120d9530b98f294a8df7b7c443fd770d28c
jswiss <swissjoshua@gmail.com>
2021-03-26 08:53:02 +0000

    mock localstorage

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,13 @@
+/* eslint-disable */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+}
+global.localStorage = localStorageMock
