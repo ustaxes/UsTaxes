f97c8e0e55acd55eb36525372bfb963a55834ba9
jswiss <swissjoshua@gmail.com>
2021-04-19 17:48:49 +0100

    Add dependent input validation tests

--- src/components/TaxPayer/SpouseAndDependent.test.tsx
+++ src/components/TaxPayer/SpouseAndDependent.test.tsx
@@ -311,13 +311,6 @@ describe('Dependents', () => {
       name: /Save/
     })
 
-    // click the save button with empty inputs
-    fireEvent.click(saveButton)
-
-    // expect six `Input is required` errors
-    const nameErrors = await screen.findAllByText('Input is required')
-    expect(nameErrors).toHaveLength(6)
-
     const closeButton = screen.getByRole('button', {
       name: /Close/
     })
@@ -408,5 +401,242 @@ describe('Dependents', () => {
 
     fireEvent.click(deleteCharlie)
     await waitFor(() => expect(screen.queryByText('Charlie Brown')).not.toBeInTheDocument())
+  })
+  it('saves and edits multiple dependents', async () => {
+    render(
+      <Provider store={store}>
+        <SpouseAndDependent />
+      </Provider>
+    )
+
+    const addButtons = screen.getAllByRole('button', {
+      name: /Add/
+    })
+
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    const [_, addDependentButton] = addButtons
+
+    fireEvent.click(addDependentButton)
+
+    const saveButton = screen.getByRole('button', {
+      name: /Save/
+    })
+
+    const studentLabel = screen.getByText('Is this person a full-time student?')
+
+    // get all inputs
+    const inputs = screen.getAllByRole('textbox')
+    const [firstNameInput, lastNameInput, ssnInput, relationInput, birthYearInput, durationInput] = inputs
+
+    // add values for each input
+    userEvent.type(firstNameInput, 'Charlie')
+    userEvent.type(lastNameInput, 'Brown')
+    fireEvent.change(ssnInput, { target: { value: '222222222' } })
+    userEvent.type(relationInput, 'Son')
+    userEvent.type(birthYearInput, '1999')
+    userEvent.type(durationInput, '12')
+    fireEvent.click(studentLabel)
+
+    fireEvent.click(saveButton)
+
+    await screen.findByText('Charlie Brown')
+    screen.getByText('222-22-2222')
+
+    const addButtonsAfterAdd = screen.getAllByRole('button', {
+      name: /Add/
+    })
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    const [_addSpouse, addAnotherDependentButton] = addButtonsAfterAdd
+
+    fireEvent.click(addAnotherDependentButton)
+    const newStudentLabel = screen.getByText('Is this person a full-time student?')
+
+    const newInputs = screen.getAllByRole('textbox')
+    const [newFirstNameInput, newLastNameInput, newSsnInput, newRelationInput, newBirthYearInput, newDurationInput] = newInputs
+
+    userEvent.type(newFirstNameInput, 'Sally')
+    userEvent.type(newLastNameInput, 'Brown')
+    fireEvent.change(newSsnInput, { target: { value: '333333333' } })
+    userEvent.type(newRelationInput, 'Daughter')
+    userEvent.type(newBirthYearInput, '2002')
+    userEvent.type(newDurationInput, '12')
+    fireEvent.click(newStudentLabel)
+
+    const newSaveButton = screen.getByRole('button', {
+      name: /Save/
+    })
+    fireEvent.click(newSaveButton)
+
+    await screen.findByText('Charlie Brown')
+    screen.getByText('222-22-2222')
+    screen.getByText('Sally Brown')
+    screen.getByText('333-33-3333')
+
+    const editButtons = screen.getAllByLabelText('edit')
+
+    const [editCharlie, editSally] = editButtons
+    fireEvent.click(editCharlie)
+
+    const filledInputs = await screen.findAllByRole('textbox')
+    const [filledFirstNameInput, filledLastNameInput, filledSsnInput, filledRelationInput, filledBirthYearInput, filledDurationInput] = filledInputs as HTMLInputElement[]
+
+    expect(filledFirstNameInput.value).toBe('Charlie')
+    expect(filledLastNameInput.value).toBe('Brown')
+    expect(filledSsnInput.value).toBe('222-22-2222')
+    expect(filledRelationInput.value).toBe('Son')
+    expect(filledBirthYearInput.value).toBe('1999')
+    expect(filledDurationInput.value).toBe('12')
+    expect(filledFirstNameInput.value).toBe('Charlie')
+
+    userEvent.type(filledFirstNameInput, '{selectall}{del}Deebo')
+    fireEvent.change(filledSsnInput, { target: { value: '777777777' } })
+
+    fireEvent.click(screen.getByRole('button', {
+      name: /Save/
+    }))
+
+    await screen.findByText('Deebo Brown')
+    await screen.findByText('777-77-7777')
+
+    const deleteButtons = screen.getAllByLabelText('delete')
+    expect(deleteButtons).toHaveLength(2)
+
+    const [deleteDeebo, deleteSally] = deleteButtons
+
+    fireEvent.click(deleteSally)
+    await waitFor(() => expect(screen.queryByText('Sally Brown')).not.toBeInTheDocument())
+
+    fireEvent.click(deleteDeebo)
+    await waitFor(() => expect(screen.queryByText('Deebo Brown')).not.toBeInTheDocument())
+  })
+
+  it('renders appropriate input errors', async () => {
+    render(
+      <Provider store={store}>
+        <SpouseAndDependent />
+      </Provider>
+    )
+
+    const addButtons = screen.getAllByRole('button', {
+      name: /Add/
+    })
+
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    const [_, addDependentButton] = addButtons
+
+    fireEvent.click(addDependentButton)
+
+    const labels = ['First Name and Initial', 'Last Name', 'SSN / TIN', 'Relationship to Taxpayer', 'Birth Year', 'How many months did you live together this year?', 'Is this person a full-time student?']
+    // Assert all form labels appear
+    for (const label of labels) {
+      screen.getByText(label)
+    }
+
+    const saveButton = screen.getByRole('button', {
+      name: /Save/
+    })
+
+    // click the save button with empty inputs
+    fireEvent.click(saveButton)
+
+    // expect six `Input is required` errors
+    const nameErrors = await screen.findAllByText('Input is required')
+    expect(nameErrors).toHaveLength(6)
+
+    const inputs = screen.getAllByRole('textbox')
+    const [firstNameInput, lastNameInput, ssnInput, relationInput, birthYearInput, durationInput] = inputs
+
+    userEvent.type(firstNameInput, '8675309')
+    fireEvent.click(saveButton)
+
+    const firstNameErrors = await screen.findAllByText('Input should only include letters and spaces')
+    expect(firstNameErrors).toHaveLength(1)
+    expect(screen.getAllByText('Input is required')).toHaveLength(5)
+
+    userEvent.type(firstNameInput, '{selectall}{del}Booker T')
+    fireEvent.click(saveButton)
+
+    await waitFor(() => expect(screen.queryByText('Input should only include letters and spaces')).not.toBeInTheDocument())
+    expect(screen.getAllByText('Input is required')).toHaveLength(5)
+
+    userEvent.type(lastNameInput, '666')
+    fireEvent.click(saveButton)
+
+    const lastNameErrors = await screen.findAllByText('Input should only include letters and spaces')
+    expect(lastNameErrors).toHaveLength(1)
+    expect(screen.getAllByText('Input is required')).toHaveLength(4)
+
+    userEvent.type(lastNameInput, '{selectall}{del}Washington')
+    fireEvent.click(saveButton)
+
+    await waitFor(() => expect(screen.queryByText('Input should only include letters and spaces')).not.toBeInTheDocument())
+    expect(screen.getAllByText('Input is required')).toHaveLength(4)
+
+    fireEvent.change(ssnInput, { target: { value: '123' } })
+    fireEvent.click(saveButton)
+
+    const ssnErrors = await screen.findAllByText('Input should be filled with 9 digits')
+    expect(ssnErrors).toHaveLength(1)
+    expect(screen.getAllByText('Input is required')).toHaveLength(3)
+
+    fireEvent.change(ssnInput, { target: { value: '' } })
+    fireEvent.change(ssnInput, { target: { value: '123456789' } })
+    fireEvent.click(saveButton)
+
+    await waitFor(() => expect(screen.queryByText('Input should be filled with 9 digits')).not.toBeInTheDocument())
+    expect(screen.getAllByText('Input is required')).toHaveLength(3)
+
+    userEvent.type(relationInput, '1111')
+    fireEvent.click(saveButton)
+
+    const relationshipErrors = await screen.findAllByText('Input should only include letters and spaces')
+    expect(relationshipErrors).toHaveLength(1)
+    expect(screen.getAllByText('Input is required')).toHaveLength(2)
+
+    userEvent.type(relationInput, '{selectall}{del}stepchild')
+    fireEvent.click(saveButton)
+
+    await waitFor(() => expect(screen.queryByText('Input should only include letters and spaces')).not.toBeInTheDocument())
+    expect(screen.getAllByText('Input is required')).toHaveLength(2)
+
+    userEvent.type(birthYearInput, 'abcd')
+    fireEvent.click(saveButton)
+
+    const inputErrors = await screen.findAllByText('Input is required')
+    expect(inputErrors).toHaveLength(2)
+
+    userEvent.type(birthYearInput, '{selectall}{del}1294')
+    fireEvent.click(saveButton)
+
+    const newBirthErrors = await screen.findAllByText('Input must be greater than or equal to 1900')
+    expect(newBirthErrors).toHaveLength(1)
+    expect(screen.getAllByText('Input is required')).toHaveLength(1)
+
+    userEvent.type(birthYearInput, '{selectall}{del}1999')
+    fireEvent.click(saveButton)
+
+    await waitFor(() => expect(screen.queryByText('Input must be greater than or equal to 1900')).not.toBeInTheDocument())
+    expect(screen.getAllByText('Input is required')).toHaveLength(1)
+
+    userEvent.type(durationInput, 'abcd')
+    fireEvent.click(saveButton)
+
+    expect(screen.getAllByText('Input is required')).toHaveLength(1)
+
+    userEvent.type(durationInput, '{selectall}{del}15')
+    fireEvent.click(saveButton)
+
+    const newDurationErrors = await screen.findAllByText('Input must be less than or equal to 12')
+    expect(newDurationErrors).toHaveLength(1)
+    expect(screen.queryAllByText('Input is required')).toHaveLength(0)
+
+    userEvent.type(durationInput, '{selectall}{del}10')
+    fireEvent.click(saveButton)
+
+    await waitForElementToBeRemoved(() => screen.queryByText('Input must be less than or equal to 12'))
+    expect(screen.queryAllByText('Input is required')).toHaveLength(0)
+
+    const deleteButton = await screen.findByLabelText('delete')
+    fireEvent.click(deleteButton)
   })
 })
