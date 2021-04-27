5b2d5442a321b79940bb8336f3c006de3799681e
Aidan Grimshaw <grimshawaidan@gmail.com>
2021-04-26 18:30:58 -0700

    changing desktop icon for windows

--- src-tauri/src/build.rs
+++ src-tauri/src/build.rs
@@ -3,9 +3,9 @@ extern crate winres;
 
 #[cfg(windows)]
 fn main() {
-  if std::path::Path::new("../../public/favicon.ico").exists() {
+  iif std::path::Path::new("icons/icon.ico").exists() {
     let mut res = winres::WindowsResource::new();
-    res.set_icon("../../public/favicon.ico");
+    res.set_icon("icons/icon.ico");
     res.compile().expect("Unable to find visual studio tools");
   } else {
     panic!("No Icon.ico found. Please add one or check the path");
