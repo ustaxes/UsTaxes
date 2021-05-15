18c7156f987070514c69d101cfdce7fc78d5b882
Zak Patterson <pattersonzak@gmail.com>
2021-05-15 14:26:15 +0900

    Fix Earned Income credit for real estate income (#283)
    * Refactor forms to better separate concerns
    
    I'm trying to separate the PDF filling from the form assembly.
    
    The functions in irsForms/Main complete all calculations and
    produce an array of forms, then pdfFiller/fillPdf takes that
    array of forms and pushes the data into the actual PDFDocuments.
    
    This way we can create the complete data model for what will be
    inserted into the PDF, and probe it for correctness and for display
    
    * Add calculation from Pub596Worksheet 1
    
    This is needed to exclude the earned income credit
    for taxpayers with net real estate income above
    the investment income level
    
    * Fix create 1040 with unitialized state bug
    
    * Fix schedule e max investment income
    
    Co-authored-by: Aidan Grimshaw <grimshawaidan@gmail.com>

--- src/irsForms/F8814.ts
+++ src/irsForms/F8814.ts
@@ -1,10 +1,19 @@
 import { TaxPayer } from '../redux/data'
 
+const unimplemented = (message: string): void =>
+  console.warn(`[Form 8814] unimplemented ${message}`)
+
 export default class F8814 {
   tp: TaxPayer
 
   constructor (tp: TaxPayer) {
     this.tp = tp
+  }
+
+  // TODO: required from schedule EIC, pub596, worksheet 1
+  l1b = (): number | undefined => {
+    unimplemented('line 1b')
+    return undefined
   }
 
   tax = (): number => 0
