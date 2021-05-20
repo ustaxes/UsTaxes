877e3f7f7ae7e0ddd65b395df899a68c92569e76
Zak Patterson <pattersonzak@gmail.com>
2021-05-20 18:23:24 +0900

    Reorganize icons for exe bundling

--- src-tauri/src/build.rs
+++ src-tauri/src/build.rs
@@ -3,7 +3,7 @@ use tauri_build::{try_build, Attributes, WindowsAttributes};
 fn main() {
   if let Err(error) = try_build(
     Attributes::new()
-      .windows_attributes(WindowsAttributes::new().window_icon_path("../../../public/favicon.ico")),
+      .windows_attributes(WindowsAttributes::new().window_icon_path("icons/favicon.ico")),
   ) {
     panic!("error found during tauri-build: {}", error);
   }
