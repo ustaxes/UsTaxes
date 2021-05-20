9929d0194e622af035ff39fc8d36523a88f80dc6
Zak Patterson <pattersonzak@gmail.com>
2021-05-20 13:56:17 +0900

    Fix build dependency, follow example from...
    https://github.com/tauri-apps/tauri/tree/tauri-v1.0.0-beta.1/examples/multiwindow/src-tauri

--- src-tauri/src/build.rs
+++ src-tauri/src/build.rs
@@ -1,1 +1,10 @@
-fn main(){}
+use tauri_build::{try_build, Attributes, WindowsAttributes};
+
+fn main() {
+  if let Err(error) = try_build(
+    Attributes::new()
+      .windows_attributes(WindowsAttributes::new().window_icon_path("../../.icons/icon.ico")),
+  ) {
+    panic!("error found during tauri-build: {}", error);
+  }
+}
