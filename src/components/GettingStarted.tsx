88df3002ae9e1152418a08caea1e79e2ae131bc5
eoncarlyle <schm4131@umn.edu>
2021-05-26 12:27:24 -0500

    Created React Elements for getting started page, edited Main module accoridngly. Copy not yet written.

---
+++ src/components/GettingStarted.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react'
+
+export default function GettingStarted (): ReactElement {
+  return (
+    <form>
+      <h2>Heading 2</h2>
+      <h4>Heading 4</h4>
+    </form>
+  )
+}
