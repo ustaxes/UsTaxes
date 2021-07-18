bb8e5215a597b80d113a55dbb0723e792e4a5c5e
Zak Patterson <pattersonzak@gmail.com>
2021-07-18 15:12:41 +0900

    Fix uncontrolled, controlled input error

--- src/components/input/LabeledInput.tsx
+++ src/components/input/LabeledInput.tsx
@@ -42,7 +42,7 @@ export function LabeledInput (props: LabeledInputProps): ReactElement {
               customInput={TextField}
               isNumericString={false}
               onValueChange={(v) => onChange(v.value)}
-              value={value}
+              value={value ?? ''}
               error={error !== undefined}
               helperText={errorMessage}
               variant="filled"
@@ -78,7 +78,7 @@ export function LabeledInput (props: LabeledInputProps): ReactElement {
                 message: patternConfig?.description ?? (required ? 'Input is required' : '')
               }
             })}
-            value={value}
+            value={value ?? ''}
             onChange={onChange}
             fullWidth={patternConfig?.format === undefined}
             helperText={error?.message}
