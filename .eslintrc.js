882d76d998419574da0b05134873f072dba8a9d9
Zak Patterson <pattersonzak@gmail.com>
2021-08-02 09:41:33 +0900

    Remove custom eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,16 +19,5 @@ module.exports = {
     sourceType: 'module',
     project: './tsconfig.json'
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
-  rules: {
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md#how-to-use
-    // note you must disable the base rule as it can report incorrect errors
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error'],
-    'react/react-in-jsx-scope': 'off',
-    // proptypes validation as warnings for now
-    // in js defining extra prop types is a little
-    // cumbersome. Remember to remove if we switch to TS
-    'react/prop-types': 1
-  }
+  plugins: ['react', '@typescript-eslint', 'prettier']
 }
