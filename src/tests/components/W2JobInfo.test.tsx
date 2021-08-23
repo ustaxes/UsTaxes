ccadc17f8f22800349d24236ce1ccc71348902c4
Iain Schmitt <iain.schmitt@relexsolutions.com>
2021-08-22 21:48:48 -0500

    Completed state modification with  modification, added second unit test

--- src/tests/components/W2JobInfo.test.tsx
+++ src/tests/components/W2JobInfo.test.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import {
   render,
   screen,
   waitFor,
 } from '@testing-library/react'
-import { Provider } from 'react-redux'
+import { useDispatch, Provider } from 'react-redux'
 import userEvent from '@testing-library/user-event'
 
 import W2JobInfo from '../../components/income/W2JobInfo'
 import { store } from '../../redux/store'
+
+import { savePrimaryPersonInfo, PrimaryPerson, Person, addSpouse} from '../../redux/actions'
+
+import {
+  Address,
+  PersonRole,
+
+} from '../../redux/data'
 
 afterEach(async () => {
   await waitFor(() => localStorage.clear())
@@ -23,27 +31,71 @@ jest.mock('redux-persist', () => {
   }
 })
 
-const IncomeW2UserInputStrings: string[] = ['Employer name', 'Occupation', 'Wages, tips, other compensation',
+const w2FormLabels: string[] = ['Employer name', 'Occupation', 'Wages, tips, other compensation',
   'Federal income tax withheld', 'Social security tax withheld', 'Medicare tax withheld', 'Employee']
 
 describe('W2Info', () => {
-  it('renders an `Add` button when no W2s have been added', () => {
+  it('renders an `Add` button when no W-2s have been added', () => {
     render(
       <Provider store={store}>
         <W2JobInfo />
       </Provider>
     )
 
-    screen.getByRole('button', {
+    // W-2 labels should not be present
+    for (const label of w2FormLabels){
+      expect(screen.queryByText(label)).toBeNull()
+    }
+
+  })
+  it('saves and edits a W-2', () => {
+    
+    // Creates, saves test taxpayer and spouse
+    const sallyAddress : Address = {
+      address: "Space Commerce Way",
+      city: "Merritt Island"
+    }
+
+    const sallyPersonInfo : PrimaryPerson = {
+      firstName: 'Sally K',
+      lastName: 'Ride',
+      ssid: '123-45-6789',
+      role: PersonRole.PRIMARY,
+      address: sallyAddress,
+      isTaxpayerDependent: false
+    }
+
+    const tamPersonInfo : Person = {
+      firstName: 'Tam E',
+      lastName: 'O\'Shaughnessy',
+      ssid: '987-65-4321',
+      role: Person
+    }
+    
+    store.dispatch(savePrimaryPersonInfo(sallyPersonInfo))
+    store.dispatch(addSpouse(tamPersonInfo))
+    render(
+      <Provider store={store}>
+        <W2JobInfo />
+      </Provider>
+    )
+
+    // Add new W-2, expect test taxpayer and spouse on employee drop-down list
+    const addButton = screen.getByRole('button', {
       name: /Add/
     })
 
-    // for(let inputString of IncomeW2UserInputStrings){
-    //   let label : any = screen.queryByLabelText(inputString)
-    //   expect(label).not.toBeInTheDocument()
-    // }
+    userEvent.click(addButton)
+    const employeeButton = screen.getByRole('combobox')
+    userEvent.click(employeeButton)
+    const sallyDisplayName = sallyPersonInfo.firstName + ' ' + sallyPersonInfo.lastName +  ' (' + sallyPersonInfo.ssid + ')'
+    const tamDisplayName = tamPersonInfo.firstName + ' ' + tamPersonInfo.lastName +  ' (' + tamPersonInfo.ssid + ')'
+    screen.getByText(sallyDisplayName)
+    screen.getByText(tamDisplayName)
 
-    IncomeW2UserInputStrings.map( inputString => (
-      expect(screen.queryByLabelText(inputString)).not.toBeInTheDocument()
-    ))})
+    // W-2 labels should now be present
+    for (const label of w2FormLabels) {
+      screen.getByText(label)
+    }
+  })
 })
