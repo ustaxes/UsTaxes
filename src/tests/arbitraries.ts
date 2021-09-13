098ad18a3570fb3fd5fde3db149c8c0ed13c06c8
Zak Patterson <pattersonzak@gmail.com>
2021-09-13 17:20:23 +0900

    Fix merge mistake

--- src/tests/arbitraries.ts
+++ src/tests/arbitraries.ts
@@ -111,6 +111,7 @@ const w2: Arbitrary<types.IncomeW2> = fc
     ]) => ({
       occupation,
       income,
+      medicareIncome,
       fedWithholding,
       employer,
       personRole: types.PersonRole.PRIMARY,
