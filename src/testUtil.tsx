6cf5e7ead3c3201b1c41bcf29eb1b95085516e5d
Zak Patterson <pattersonzak@gmail.com>
2021-09-18 07:37:06 +0900

    Fix menu test

--- src/testUtil.tsx
+++ src/testUtil.tsx
@@ -3,6 +3,9 @@ import { render, RenderResult } from '@testing-library/react'
 
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ViewportProvider } from 'ustaxes/hooks/Viewport'
+import { createStoreUnpersisted } from './redux/store'
+import { blankState } from './redux/reducer'
+import { Provider } from 'react-redux'
 
 export const resizeWindow = (x: number, y: number): void => {
   global.innerWidth = x
@@ -12,7 +15,9 @@ export const resizeWindow = (x: number, y: number): void => {
 
 export const renderWithProviders = (ui: ReactElement): RenderResult =>
   render(
-    <Router>
-      <ViewportProvider>{ui}</ViewportProvider>
-    </Router>
+    <Provider store={createStoreUnpersisted(blankState)}>
+      <Router>
+        <ViewportProvider>{ui}</ViewportProvider>
+      </Router>
+    </Provider>
   )
