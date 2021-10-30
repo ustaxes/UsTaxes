1c8d418d01c6422b68c14d3045c0baa05e2eb8ab
Zak Patterson <pattersonzak@gmail.com>
2021-10-30 10:23:05 +0900

    Add state reset and error for filing status
    If user has single saved and then adds a spouse, we show an
    error and remind them to select a new status.
    
    If a user has MFJ selected and deletes a spouse, we show an
    error and remind them to select a new status.
    
    Form state in these cases is now reset to empty, and this way
    react-hook-form properly shows error message and prevents
    submitting data.

--- src/components/TaxPayer/SpouseAndDependent.tsx
+++ src/components/TaxPayer/SpouseAndDependent.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 
 import { useForm, FormProvider } from 'react-hook-form'
 import { useDispatch, useSelector } from 'react-redux'
@@ -30,8 +30,9 @@ import {
 import { PersonFields } from './PersonFields'
 import { FormListContainer } from 'ustaxes/components/FormContainer'
 import { usePager } from 'ustaxes/components/pager'
-import { Grid } from '@material-ui/core'
+import { Box, Grid } from '@material-ui/core'
 import { Person } from '@material-ui/icons'
+import { Alert } from '@material-ui/lab'
 
 interface UserPersonForm {
   firstName: string
@@ -209,13 +210,15 @@ const SpouseAndDependent = (): ReactElement => {
     return state.information.taxPayer
   })
 
+  const [error, setError] = useState<ReactElement | undefined>(undefined)
+
   const { onAdvance, navButtons } = usePager()
 
-  const methods = useForm<{ filingStatus: FilingStatus }>({
-    defaultValues: { filingStatus: taxPayer.filingStatus }
+  const methods = useForm<{ filingStatus: FilingStatus | '' }>({
+    defaultValues: { filingStatus: taxPayer.filingStatus ?? '' }
   })
 
-  const { handleSubmit } = methods
+  const { handleSubmit, getValues, reset, watch } = methods
 
   // const variable dispatch to allow use inside function
   const dispatch = useDispatch()
@@ -224,6 +227,32 @@ const SpouseAndDependent = (): ReactElement => {
     dispatch(saveFilingStatusInfo(formData.filingStatus))
     onAdvance()
   }
+
+  const allowedFilingStatuses = filingStatuses(taxPayer)
+
+  const currentFilingStatus = getValues().filingStatus
+
+  const newValue = watch()
+
+  useEffect(() => {
+    if (
+      currentFilingStatus !== '' &&
+      !allowedFilingStatuses.includes(currentFilingStatus)
+    ) {
+      reset({ filingStatus: '' })
+      setError(
+        <Box paddingTop={2}>
+          <Alert severity="warning">
+            Filing status was set to {FilingStatusTexts[currentFilingStatus]}{' '}
+            which is no longer allowed due to your inputs. Make another
+            selection.
+          </Alert>
+        </Box>
+      )
+    } else if (currentFilingStatus !== '' || newValue.filingStatus !== '') {
+      setError(undefined)
+    }
+  })
 
   const page = (
     <form tabIndex={-1} onSubmit={handleSubmit(onSubmit)}>
@@ -237,13 +266,14 @@ const SpouseAndDependent = (): ReactElement => {
       <Grid container spacing={2}>
         <GenericLabeledDropdown<FilingStatus>
           label="Filing Status"
-          dropDownData={filingStatuses(taxPayer)}
+          dropDownData={allowedFilingStatuses}
           valueMapping={(x) => x}
           keyMapping={(x, i) => i}
           textMapping={(status) => FilingStatusTexts[status]}
           name="filingStatus"
         />
       </Grid>
+      {error}
       {navButtons}
     </form>
   )
