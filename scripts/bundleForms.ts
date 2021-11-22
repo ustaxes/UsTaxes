d8430d1efdaa4c98110425066fc4a51c918321de
Zak Patterson <pattersonzak@gmail.com>
2021-11-22 13:55:47 +0900

    Add version check for node script

--- scripts/bundleForms.ts
+++ scripts/bundleForms.ts
@@ -1,6 +1,20 @@
 import fs from 'fs/promises'
 import path from 'path'
 
+// fs.cp since 16.7
+const requiredNodeVersion = [16, 7]
+
+const checkVersion = () => {
+  const [M, m] = process.version.split('.').map((x) => parseInt(x, 10))
+  const [R, r] = requiredNodeVersion
+  if (M < R || (M === R && m < r)) {
+    console.error('Required node version not found.')
+    console.error(`Required: >= ${requiredNodeVersion.join('.')}`)
+    console.error(`Found:       ${process.version}`)
+    process.exit(1)
+  }
+}
+
 const years = ['Y2020']
 const src = (year: string): string =>
   path.join(__dirname, `../ustaxes-forms/${year}/public/`)
@@ -19,6 +33,7 @@ const copyYear = async (year: string): Promise<void> => {
 }
 
 if (require.main === module) {
+  checkVersion()
   Promise.all(years.map(async (y) => copyYear(y))).then(() =>
     console.log('done')
   )
