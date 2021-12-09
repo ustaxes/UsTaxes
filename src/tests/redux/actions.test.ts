dd599822a048cb65f4ad34d26d7e5463ed0ab21a
Zak Patterson <pattersonzak@gmail.com>
2021-12-09 14:48:59 +0900

    Fix bad import

--- src/tests/redux/actions.test.ts
+++ src/tests/redux/actions.test.ts
@@ -1,6 +1,6 @@
-import * as arbitraries from 'ustaxes-core/src/tests/arbitraries'
+import * as arbitraries from 'ustaxes-core/tests/arbitraries'
 import * as fc from 'fast-check'
-import * as validators from 'ustaxes-core/src/data/validate'
+import * as validators from 'ustaxes-core/data/validate'
 import Ajv from 'ajv'
 
 const ajv = new Ajv()
