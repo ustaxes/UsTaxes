375b2749c5404044a0c8ac419a726a5e5230ed24
Connor Skennerton <c.skennerton@gmail.com>
2022-01-01 23:38:53 -0800

    commented out f1040 test that required less tax than income
    Commented it out because it will fail under certain seemingly legit
    circumstances. For example, if there is no income from W2 or 1099
    but an excess distribution from a HSA is done, then there will be
    more tax than income.

--- src/forms/Y2021/tests/f1040.test.ts
+++ src/forms/Y2021/tests/f1040.test.ts
@@ -13,18 +13,22 @@ beforeAll(() => {
 describe('f1040', () => {
   commonTests.run()
 
-  it('should never produce higher tax than income', async () => {
-    await testKit.with1040Assert(async (forms) => {
-      const f1040 = commonTests.findF1040(forms)
-      expect(f1040).not.toBeUndefined()
-      if (f1040 !== undefined) {
-        // tax is less than taxable income
-        if (f1040.l15() ?? 0 > 0) {
-          expect(f1040.l24() ?? 0).toBeLessThan(f1040.l15() ?? 0)
-        } else {
-          expect(f1040.l24() ?? 0).toEqual(0)
-        }
-      }
-    })
-  })
+  // TODO: revisit this test. Commented it out because it will fail
+  // under certain seemingly legit circumstances. For example, if there
+  // is no income from W2 or 1099 but an excess distribution from a HSA
+  // is done, then there will be more tax than income.
+  // it('should never produce higher tax than income', async () => {
+  //   await testKit.with1040Assert(async (forms) => {
+  //     const f1040 = commonTests.findF1040(forms)
+  //     expect(f1040).not.toBeUndefined()
+  //     if (f1040 !== undefined) {
+  //       // tax is less than taxable income
+  //       if (f1040.l15() ?? 0 > 0) {
+  //         expect(f1040.l24() ?? 0).toBeLessThan(f1040.l15() ?? 0)
+  //       } else {
+  //         expect(f1040.l24() ?? 0).toEqual(0)
+  //       }
+  //     }
+  //   })
+  // })
 })
