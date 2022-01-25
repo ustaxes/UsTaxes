c40a232019949813222c7e2d7036183f7fef6ac3
Zak Patterson <pattersonzak@gmail.com>
2022-01-24 23:17:33 -0600

    Switch to script for deployment

---
+++ scripts/netlify-deploy.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Modified from https://raw.githubusercontent.com/jsmrcaga/action-netlify-deploy/master/entrypoint.sh
+# This script was being run inside a docker container, which is a bit of extra overhead,
+# we can make a bunch of simplifications and just run it inside github's (already) containerized
+# environment.
+
+
+set -e
+
+# Install netlify globally before NVM to prevent EACCESS issues
+npm i -g netlify-cli
+
+# Save its exec path to run later
+NETLIFY_CLI=$(which netlify)
+
+NETLIFY_AUTH_TOKEN=$1
+NETLIFY_SITE_ID=$2
+NETLIFY_DEPLOY_TO_PROD=$3
+BUILD_DIRECTORY=$4
+FUNCTIONS_DIRECTORY=$5
+DEPLOY_ALIAS=$6
+DEPLOY_MESSAGE=$7
+
+# Export token to use with netlify's cli
+export NETLIFY_SITE_ID="$NETLIFY_SITE_ID"
+export NETLIFY_AUTH_TOKEN="$NETLIFY_AUTH_TOKEN"
+
+COMMAND="$NETLIFY_CLI deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message=$DEPLOY_MESSAGE"
+
+if [[ $NETLIFY_DEPLOY_TO_PROD == "true" ]]
+then
+	COMMAND+=" --prod"
+elif [[ -n $DEPLOY_ALIAS ]]
+then
+	COMMAND+=" --alias $DEPLOY_ALIAS"
+fi
+
+# Deploy with netlify
+OUTPUT=$(sh -c "$COMMAND")
+
+NETLIFY_OUTPUT=$(echo "$OUTPUT")
+NETLIFY_PREVIEW_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*(--)[a-zA-Z0-9./?=_-]*') #Unique key: --
+NETLIFY_LOGS_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://app.netlify.com/[a-zA-Z0-9./?=_-]*') #Unique key: app.netlify.com
+NETLIFY_LIVE_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' | grep -Eov "netlify.com") #Unique key: don't containr -- and app.netlify.com
+
+echo "::set-output name=NETLIFY_OUTPUT::$NETLIFY_OUTPUT"
+echo "::set-output name=NETLIFY_PREVIEW_URL::$NETLIFY_PREVIEW_URL"
+echo "::set-output name=NETLIFY_LOGS_URL::$NETLIFY_LOGS_URL"
+echo "::set-output name=NETLIFY_LIVE_URL::$NETLIFY_LIVE_URL"
