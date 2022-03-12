96399e0e8a3a0d93d7a7288b7ed33dfaab96b29e
Zak Patterson <pattersonzak@gmail.com>
2022-03-12 15:53:56 +0900

    Update DatePicker component for new picker in lab

--- src/components/input/DatePicker.tsx
+++ src/components/input/DatePicker.tsx
@@ -1,14 +1,9 @@
 import { ReactElement } from 'react'
 import { FormControl, FormLabel, Grid, TextField } from '@mui/material'
 import { Controller, useFormContext } from 'react-hook-form'
-import { DesktopDatePicker as MuiDatePicker } from '@mui/material'
 import ConditionallyWrap from 'ustaxes/components/ConditionallyWrap'
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker as MuiDatePicker
-} from '@material-ui/pickers'
-import DateFnsUtils from '@date-io/date-fns'
 import { DatePickerProps } from './types'
+import { DesktopDateTimePicker as MuiDatePicker } from '@mui/lab'
 
 export function DatePicker(props: DatePickerProps): ReactElement {
   const {
@@ -38,16 +33,14 @@ export function DatePicker(props: DatePickerProps): ReactElement {
           <div className={classes.root}>
             <FormControl component="fieldset">
               <FormLabel>{label}</FormLabel>
-              <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <MuiDatePicker
-                  minDate={minDate}
-                  maxDate={maxDate}
-                  value={value}
-                  onChange={onChange}
-                  format="MM/dd/yyyy"
-                  emptyLabel="mm/dd/yyyy"
-                />
-              </MuiPickersUtilsProvider>
+              <MuiDatePicker
+                minDate={minDate}
+                maxDate={maxDate}
+                value={value}
+                onChange={onChange}
+                inputFormat="MM/dd/yyyy"
+                renderInput={(params) => <TextField {...params} />}
+              />
             </FormControl>
           </div>
         )}
