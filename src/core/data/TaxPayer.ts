993c400f46cb68ee316b55405b2f35f33bca7e06
Connor Skennerton <c.skennerton@gmail.com>
2022-04-06 00:14:45 -0700

    Refactor the TaxPayer class to better implement the TaxPayer interface
    - no longer assign the object as 'tp' which created awkward calls
      to `taxpayer.tp`, instead get keys directly like `taxpayer.dependents`
    - Add in `isBlind` and `dateOfBirth` to the `Person` interface and
      make sure that the TaxPayer class represents them as `Date`.
      This change has the unfortunate effect of duplicating these
      interfaces so that the `dateOfBirth` can be a `Date` type while
      the structure in the `Information` interface is a `string`. The
      reason for this is that serializing the `Information` object doesn't
      play nice with date types and it does not appear possible to
      extend a generic interface, which would have allowed us to pass in
      the type as a parameter like what is done with `HealthSavingsAccounts`.

--- src/core/data/TaxPayer.ts
+++ src/core/data/TaxPayer.ts
@@ -1,18 +1,78 @@
-import { Person, TaxPayer as TP } from '.'
+import { TaxPayer as InfoTaxPayer, PersonRole, Address, FilingStatus } from '.'
+
+interface Person {
+  firstName: string
+  lastName: string
+  ssid: string
+  role: PersonRole
+  dateOfBirth: Date
+  isBlind: boolean
+}
+
+interface QualifyingInformation {
+  numberOfMonths: number
+  isStudent: boolean
+}
+
+export interface Dependent extends Person {
+  relationship: string
+  qualifyingInfo?: QualifyingInformation
+}
+
+interface PrimaryPerson extends Person {
+  address: Address
+  isTaxpayerDependent: boolean
+}
+
+interface Spouse extends Person {
+  isTaxpayerDependent: boolean
+}
+
+interface ContactInfo {
+  contactPhoneNumber?: string
+  contactEmail?: string
+}
+
+interface TP extends ContactInfo {
+  filingStatus?: FilingStatus
+  primaryPerson?: PrimaryPerson
+  spouse?: Spouse
+  dependents: Dependent[]
+}
 
 /**
  * Used to augment the TaxPayer data interface with some convenience
  * methods.
  */
-export default class TaxPayer {
-  tp: TP
+export default class TaxPayer implements TP {
+  primaryPerson?: PrimaryPerson | undefined
+  spouse?: Spouse | undefined
+  dependents: Dependent[]
+  filingStatus?: FilingStatus | undefined
 
-  constructor(tp: TP) {
-    this.tp = tp
+  constructor(tp: InfoTaxPayer) {
+    if (tp.primaryPerson !== undefined) {
+      this.primaryPerson = {
+        ...tp.primaryPerson,
+        dateOfBirth: new Date(tp.primaryPerson.dateOfBirth)
+      }
+    }
+
+    if (tp.spouse !== undefined) {
+      this.spouse = {
+        ...tp.spouse,
+        dateOfBirth: new Date(tp.spouse.dateOfBirth)
+      }
+    }
+
+    this.dependents = tp.dependents.map((d) => {
+      return { ...d, dateOfBirth: new Date(d.dateOfBirth) }
+    })
+    this.filingStatus = tp.filingStatus
   }
 
   namesString = (): string => {
-    const ps: Person[] = [this.tp.primaryPerson, this.tp.spouse]
+    const ps: Person[] = [this.primaryPerson, this.spouse]
       .filter((p: Person | undefined) => p !== undefined)
       .map((p: Person | undefined) => p as Person)
 
