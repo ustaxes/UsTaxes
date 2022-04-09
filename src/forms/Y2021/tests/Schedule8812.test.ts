8646adfeec5e8c378c638b26bd7c671a8f409654
Zak Patterson <pattersonzak@gmail.com>
2022-04-10 06:27:50 +0900

    Fix 8812 Math #1093 (#1095)
    * Refactor test function for reuse
    
    Improve refactoring
    
    * Add test for 8812 cases
    
    Add test
    
    * Fix math typos in 8812 L5
    
    These were causing child tax credit with no dependents
    
    * Fix Part 3 math typos
    
    * Format
    
    * Fix backwards subtraction
    
    * Fix overflow issue
    
    For very large numbers > 9E15, % operator fails.
    
    So if property based tests generate very large numbers
    and we attempt to calculate the next multiple of 1000, we
    cannot reliably test it as a multiple of 1000 using %.

---
+++ src/forms/Y2021/tests/Schedule8812.test.ts
@@ -0,0 +1,45 @@
+import { commonTests } from '.'
+import Schedule8812 from '../irsForms/Schedule8812'
+import F1040 from '../irsForms/F1040'
+
+const withSchedule8812 = async (
+  f: (f1040: F1040, s8812: Schedule8812) => void
+): Promise<void> =>
+  await commonTests.withValid1040(
+    (f1040: F1040): void => {
+      if (f1040.schedule8812 !== undefined) {
+        f(f1040, f1040.schedule8812)
+      }
+    },
+    // Add filter to info property so we're only testing in the domain
+    // we care about.
+    (info) => info.taxPayer.dependents.length > 0
+  )
+
+describe('Schedule 8812', () => {
+  it('should be attached with qualifiying dependents', async () => {
+    await commonTests.withValid1040(async (f1040) => {
+      // If there are qualifying dependents, we must have a schedule 8812
+      if (
+        (f1040.childTaxCreditWorksheet?.numberQualifyingChildren() ?? 0) > 0
+      ) {
+        expect(f1040.schedule8812).not.toBe(undefined)
+      }
+    })
+  })
+
+  it('should not produce line 5 with no dependents', async () => {
+    await withSchedule8812((f1040, s8812) => {
+      // If Schedule A is attached, the deduction should be greater than the standard deduction
+      if (s8812.l4a() === 0) {
+        expect(s8812.l5()).toEqual(0)
+      }
+    })
+  })
+
+  it('should show a multiple of 1000 at l10', async () => {
+    await withSchedule8812((f1040, s8812) => {
+      expect(s8812.l10() % 1000).toEqual(0)
+    })
+  })
+})
