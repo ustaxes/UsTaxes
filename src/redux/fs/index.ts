2fd8f9da9bbb485df4e50aa890040831e49fa07a
Zak Patterson <pattersonzak@gmail.com>
2022-04-28 01:16:00 +0900

    Refactor to make load/save testable, add fail test

--- src/redux/fs/index.ts
+++ src/redux/fs/index.ts
@@ -1,8 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import { deserializeTransform, serializeTransform, USTState } from '../store'
 import Load from './Load'
 
-const download = (filename: string, text: string): void => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
+export const stateToString = (state: any): string =>
+  JSON.stringify(serializeTransform(state))
+
+export const stringToState = (str: string): USTState =>
+  deserializeTransform(JSON.parse(str)) as USTState
+
+export const download = (filename: string, text: string): void => {
   const element = document.createElement('a')
   element.setAttribute(
     'href',
@@ -18,7 +26,7 @@ const download = (filename: string, text: string): void => {
   document.body.removeChild(element)
 }
 
-const loadFile = async (file: File): Promise<string> => {
+export const loadFile = async (file: File): Promise<string> => {
   const fileReader: FileReader = new FileReader()
 
   return new Promise((resolve, reject) => {
@@ -39,4 +47,4 @@ const loadFile = async (file: File): Promise<string> => {
   })
 }
 
-export { download, loadFile, Load }
+export { Load }
