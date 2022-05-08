fe0b403c5e21622df8d8c99e2d6b9b7d7dd905d2
Connor Skennerton <c.skennerton@gmail.com>
2022-05-07 23:24:15 -0700

    Fix failing test for Schedule A
    There was a test that assumed certain amounts for the standard
    deduction that no longer hold true with the allowances for age and
    blindness.
    
    Refactored that test to be more resilient to these changes.

--- src/forms/Y2021/tests/ScheduleA.test.ts
+++ src/forms/Y2021/tests/ScheduleA.test.ts
@@ -15,15 +15,11 @@ describe('ScheduleA', () => {
 
   it('should be attached if deduction is more than standard', async () => {
     await commonTests.withValid1040((f1040, fs) => {
-      const standardDeduction =
-        federalBrackets.ordinary.status[fs].deductions[0].amount
+      const standardDeduction = f1040.standardDeduction() ?? 0
 
       // If the deduction is more than standard, we must have a schedule A
       // Note dependents of other taxpayers may still itemize.
-      if (
-        (f1040.l12a() ?? 0) >
-        Math.min(standardDeduction, f1040.standardDeduction() ?? 0)
-      ) {
+      if ((f1040.l12a() ?? 0) > standardDeduction) {
         expect(f1040.scheduleA).not.toBe(undefined)
       }
     })
