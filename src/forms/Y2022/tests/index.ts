a1fb6d942a484b1c3d81f76e9e71d39cf0537393
Aidan Grimshaw <grimshawaidan@gmail.com>
2023-03-14 23:04:31 -0700

    update test year

--- src/forms/Y2022/tests/index.ts
+++ src/forms/Y2022/tests/index.ts
@@ -2,14 +2,14 @@ import CommonTests, { FormTestInfo } from 'ustaxes/forms/tests/CommonTests'
 import TestKit from 'ustaxes/forms/tests/TestKit'
 import F1040 from '../irsForms/F1040'
 
-export const testKit = new TestKit('Y2021')
+export const testKit = new TestKit('Y2022')
 
-class FormTestInfo2021 extends FormTestInfo<F1040> {
+class FormTestInfo2022 extends FormTestInfo<F1040> {
   getAssets = (f1040: F1040) => f1040.assets
   getInfo = (f1040: F1040) => f1040.info
 }
 
 export const commonTests = new CommonTests<F1040>(
   testKit,
-  new FormTestInfo2021()
+  new FormTestInfo2022()
 )
