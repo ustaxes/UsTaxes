8cab18177172628180a57f900dbdaf0646190628
Ellie Peterson <ekp2h92@gmail.com>
2026-03-25 15:51:26 -0400

    ✨ Update Pub596 worksheet and fix math bug in it. line 10 had subtraction backwards worksheet 2 was never added (fair, tbh), but now exists.

--- src/forms/Y2025/irsForms/F8814.ts
+++ src/forms/Y2025/irsForms/F8814.ts
@@ -6,8 +6,20 @@ export default class F8814 extends F1040Attachment {
   tag: FormTag = 'f8814'
   sequenceIndex = 999
 
-  // TODO: required from schedule EIC, pub596, worksheet 1
+  // TODO: required from schedule EIC, pub596, worksheet 2
+  l1a = (): number | undefined => undefined
+
+  // TODO: required from schedule EIC, pub596, worksheet 1, worksheet 2
   l1b = (): number | undefined => undefined
+
+  // TODO: required from pub596, worksheet 2
+  l2a = (): number | undefined => undefined
+
+  // TODO: required from pub596, worksheet 2
+  l2b = (): number | undefined => undefined
+
+  // TODO: required from pub596, worksheet 2
+  l12 = (): number | undefined => undefined
 
   tax = (): number => 0
 
